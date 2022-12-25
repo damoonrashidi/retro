0ab54b2f20d5074b840ed9a8dc081fb61bc03368
Damoon Rashidi <damoon.rashidi@gmail.com>
2022-12-25 17:08:56 +0100

    Fix insertion bug

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -16,7 +16,7 @@ pub mod vote_handler;
 pub fn handle_input(input: &Input, state: &mut State, textarea: &mut TextArea<'_>) {
     handle_show_help(input, state);
     handle_vote(input, state);
-    handle_mode(input, state);
     handle_group(input, state);
     handle_insert(input, state, textarea);
+    handle_mode(input, state);
 }
